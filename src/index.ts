e26aaa833a7ff055b2ef7c83485b2beaa4eabc0a
0xgareth <gareth@garethv.xyz>
2022-05-26 21:28:08 +0100

    const

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import { MikroORM } from "@mikro-orm/core"
+import { __prod___ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        user: 'postgres',
+        password: 'postgres',
+        type: 'postgresql',
+        debug: !__prod___
+    });
+    console.log(orm);
+}
+
+main();
